dc7abcb80f11b3fedbdfb25f0969515adc390fa5
Andrew Reusch <areusch@gmail.com>
2023-02-27 12:31:12 -0800

    test dns

--- src/relax/utils.cc
+++ src/relax/utils.cc
@@ -15,6 +15,8 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
+ * DNS TESTING
  */
 
 #include <tvm/relax/expr_functor.h>
