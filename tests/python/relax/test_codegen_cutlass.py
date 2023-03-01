d2597d86832317f492a527ed73c6da6793a53e84
Lite Ye <yelite958@gmail.com>
2023-02-28 23:45:19 -0500

    Add batch matmul support to Relax CUTLASS BYOC

--- tests/python/relax/test_codegen_cutlass.py
+++ tests/python/relax/test_codegen_cutlass.py
@@ -14,16 +14,16 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import math
-from typing import List, Tuple
-
 import numpy as np
 import pytest
+import scipy
 
 import tvm
 import tvm.testing
 from tvm import relax, relay
+from tvm.contrib.cutlass.build import is_valid_for_cutlass_matmul
 from tvm.relax.backend import get_patterns_with_prefix
+from tvm.relax.backend.contrib.cutlass import partition_for_cutlass
 from tvm.script import relax as R
 
 
@@ -50,78 +50,6 @@ def get_relay_conv2d_bias_relu(
         )
         + bias
     )
-
-
-def get_relay_matmul(
-    x_shape,
-    y_shape,
-    x_dtype="float16",
-    y_dtype="float16",
-    out_dtype="float16",
-):
-    x = relay.var("x", shape=x_shape, dtype=x_dtype)
-    y = relay.var("y", shape=y_shape, dtype=y_dtype)
-    return relay.nn.dense(x, y, out_dtype=out_dtype)
-
-
-def get_relay_matmul_bias(
-    x_shape,
-    y_shape,
-    x_dtype="float16",
-    y_dtype="float16",
-    bias_dtype="float16",
-    out_dtype="float16",
-):
-    bias = relay.var("bias", shape=(y_shape[0],), dtype=bias_dtype)
-    return relay.nn.bias_add(
-        get_relay_matmul(
-            x_shape,
-            y_shape,
-            x_dtype,
-            y_dtype,
-            out_dtype,
-        ),
-        bias,
-    )
-
-
-def get_relay_matmul_bias_relu(
-    x_shape,
-    y_shape,
-    x_dtype="float16",
-    y_dtype="float16",
-    bias_dtype="float16",
-    out_dtype="float16",
-):
-    return relay.nn.relu(
-        get_relay_matmul_bias(
-            x_shape,
-            y_shape,
-            x_dtype,
-            y_dtype,
-            bias_dtype,
-            out_dtype,
-        )
-    )
-
-
-def get_relay_matmul_bias_gelu(
-    x_shape,
-    y_shape,
-    x_dtype="float16",
-    y_dtype="float16",
-    bias_dtype="float16",
-    out_dtype="float16",
-):
-    bias_add = get_relay_matmul_bias(x_shape, y_shape, x_dtype, y_dtype, bias_dtype, out_dtype)
-    mul = bias_add * relay.const((1.0 / math.sqrt(2.0)), dtype=out_dtype)
-    if out_dtype == "float16":
-        erf = relay.cast(relay.op.erf(relay.cast(mul, "float32")), "float16")
-    else:
-        erf = relay.op.erf(mul)
-    mul_half = erf * relay.const(0.5, dtype=out_dtype)
-    add = mul_half + relay.const(0.5, dtype=out_dtype)
-    return add * bias_add
 
 
 def get_relay_conv2d_relu_x2(
@@ -161,7 +89,7 @@ def get_relay_ref(relay_expr, *args):
         relay_mod = seq(relay_mod)
 
     return (
-        relay.create_executor("graph", mod=relay_mod, device=tvm.gpu(0), target="cuda")
+        relay.create_executor("graph", mod=relay_mod, device=tvm.cuda(0), target="cuda")
         .evaluate()(*args)
         .numpy()
     )
@@ -221,22 +149,16 @@ pytestmark = [cutlass_enabled]
 
 def get_result_with_relax_cutlass_offload(mod, *args):
     patterns = [(entry.name, entry.pattern) for entry in get_patterns_with_prefix("cutlass")]
-
     assert len(patterns) != 0, "Cannot find cutlass patterns"
 
-    seq = tvm.transform.Sequential(
-        [
-            relax.transform.FuseOpsByPattern(patterns, annotate_codegen=True),
-            relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}}),
-        ]
-    )
-
-    mod = seq(mod)
+    mod = partition_for_cutlass(mod)
+    codegen_pass = relax.transform.RunCodegen({"cutlass": {"sm": 80, "find_first_valid": True}})
+    mod = codegen_pass(mod)
 
     target = tvm.target.Target("cuda")
     ex = relax.build(mod, target)
 
-    dev = tvm.gpu(0)
+    dev = tvm.cuda(0)
     vm = relax.VirtualMachine(ex, dev)
 
     return vm["main"](*(tvm.nd.array(arg, dev) for arg in args)).numpy()
@@ -255,13 +177,48 @@ def test_conv2d_offload():
     tvm.testing.assert_allclose(out, ref, rtol=1e-5, atol=1e-5)
 
 
+def test_kernel_sharing():
+    data_np = np.random.randn(16, 32, 32, 16).astype("float16")
+    weight1_np = np.random.randn(16, 3, 3, 16).astype("float16")
+    weight2_np = np.random.randn(16, 3, 3, 16).astype("float16")
+
+    out = get_result_with_relax_cutlass_offload(Conv2dx2, data_np, weight1_np, weight2_np)
+
+    relay_expr = get_relay_conv2d_relu_x2(data_np.shape, weight1_np.shape)
+    ref = get_relay_ref(relay_expr, data_np, weight1_np, weight2_np)
+
+    tvm.testing.assert_allclose(out, ref, rtol=1e-5, atol=1e-5)
+
+
+def get_reference_matmul_result(x, y, bias=None, transposed_y=False, activation=None):
+    if transposed_y:
+        y = np.swapaxes(y, -2, -1)
+
+    result = x @ y
+
+    if bias is not None:
+        result += bias
+
+    if activation is None:
+        pass
+    elif activation is R.nn.relu:
+        result = np.maximum(result, 0)
+    elif activation is R.nn.gelu:
+        result = 0.5 * result * (1 + scipy.special.erf(result / np.sqrt(2)))
+    else:
+        raise ValueError(f"Unknown activation function {activation}")
+
+    return result
+
+
 def get_relax_matmul_module(x, y, transposed_y=False, with_bias=False, activation=None):
-    m, k = x.shape
+    m, k = x.shape[-2:]
     if transposed_y:
         n = y.shape[-2]
     else:
         n = y.shape[-1]
     dtype = str(x.dtype)
+    y_shape = y.shape
 
     from tvm.script.ir_builder import IRBuilder
     from tvm.script.ir_builder import relax as relax_builder
@@ -276,7 +233,8 @@ def get_relax_matmul_module(x, y, transposed_y=False, with_bias=False, activatio
 
             with R.dataflow() as frame:
                 if transposed_y:
-                    y = R.emit(R.permute_dims(y))
+                    axes = list(range(len(y_shape) - 2)) + [-1, -2]
+                    y = R.emit(R.permute_dims(y, axes=axes))
                 result = R.emit(R.matmul(x, y, out_dtype=dtype))
                 if with_bias:
                     result = R.emit(result + bias)
@@ -290,152 +248,139 @@ def get_relax_matmul_module(x, y, transposed_y=False, with_bias=False, activatio
     return tvm.IRModule({"main": func})
 
 
-@pytest.fixture(params=["float16"])
-def target_dtype(request):
-    return request.param
-
-
-@pytest.fixture(
-    params=[
-        # M, K, N
-        (32, 6, 16),
-        (29, 17, 19),
-        (64, 512, 1024),
-    ]
+@pytest.mark.parametrize(
+    "x_shape, y_shape, transpose_y",
+    [
+        # Regular
+        ((32, 6), (6, 16), False),
+        # Transposed
+        ((4, 16), (16, 128), True),
+        ((35, 8), (8, 8), True),
+        # 3D x 3D
+        ((6, 32, 8), (6, 8, 10), False),
+        ((6, 32, 8), (6, 8, 10), True),
+        # 3D x 2D
+        ((6, 32, 8), (8, 10), False),
+        ((10, 16, 8), (8, 10), True),
+        # 2D x 3D
+        ((32, 8), (10, 8, 10), False),
+        ((32, 8), (10, 8, 10), True),
+        # ND x 2D
+        ((3, 6, 32, 8), (8, 10), False),
+        # 2D x ND
+        ((32, 8), (5, 3, 8, 10), False),
+        # ND x ND
+        ((5, 3, 32, 8), (5, 3, 8, 10), True),
+        ((3, 2, 4, 16, 15), (1, 1, 15, 2), True),
+        ((1, 1, 16, 15), (3, 2, 4, 15, 2), False),
+    ],
 )
-def matmul_size(request):
-    return request.param
+@pytest.mark.parametrize(
+    "with_bias, activation",
+    [
+        (True, None),
+        (False, None),
+        (True, R.nn.relu),
+        (True, R.nn.gelu),
+    ],
+    ids=[
+        "no_bias",
+        "biased",
+        "biased_relu",
+        "biased_gelu",
+    ],
+)
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        "float16",
+    ],
+)
+def test_matmul_offload(
+    x_shape,
+    y_shape,
+    transpose_y,
+    with_bias,
+    activation,
+    dtype,
+):
+    x = np.random.randn(*x_shape).astype(dtype)
+    y = np.random.randn(*y_shape).astype(dtype)
 
+    if transpose_y:
+        y = np.swapaxes(y, -2, -1)
 
-@pytest.fixture
-def matmul_x(matmul_size, target_dtype):
-    m, k, _ = matmul_size
-    return np.random.randn(m, k).astype(target_dtype)
+    if with_bias:
+        bias = np.random.randn(y_shape[-1]).astype(dtype)
+        args = (x, y, bias)
+    else:
+        bias = None
+        args = (x, y)
 
+    ref = get_reference_matmul_result(x, y, bias, transposed_y=transpose_y, activation=activation)
 
-@pytest.fixture
-def matmul_y(matmul_size, target_dtype):
-    _, k, n = matmul_size
-    return np.random.randn(k, n).astype(target_dtype)
-
-
-@pytest.fixture
-def matmul_bias(matmul_size, target_dtype):
-    _, _, n = matmul_size
-    return np.random.randn(n).astype(target_dtype)
-
-
-def test_matmul_offload(matmul_x, matmul_y):
-    x, y = matmul_x, matmul_y
-
-    mod = get_relax_matmul_module(x, y)
-    out = get_result_with_relax_cutlass_offload(mod, x, y)
-    ref_relay_expr = get_relay_matmul(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose())
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
-
-
-def test_matmul_bias_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
-
-    mod = get_relax_matmul_module(x, y, with_bias=True)
-    out = get_result_with_relax_cutlass_offload(mod, x, y, bias)
-
-    ref_relay_expr = get_relay_matmul_bias(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
-
-
-def test_matmul_bias_relu_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
-
-    mod = get_relax_matmul_module(x, y, with_bias=True, activation=R.nn.relu)
-    out = get_result_with_relax_cutlass_offload(mod, x, y, bias)
-
-    ref_relay_expr = get_relay_matmul_bias_relu(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
-
-
-def test_matmul_bias_gelu_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
-
-    mod = get_relax_matmul_module(x, y, with_bias=True, activation=R.nn.gelu)
-    out = get_result_with_relax_cutlass_offload(mod, x, y, bias)
-
-    ref_relay_expr = get_relay_matmul_bias_gelu(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
+    mod = get_relax_matmul_module(
+        x, y, with_bias=with_bias, transposed_y=transpose_y, activation=activation
+    )
+    out = get_result_with_relax_cutlass_offload(mod, *args)
 
     tvm.testing.assert_allclose(out, ref, rtol=1e-2, atol=1e-3)
 
 
-def test_kernel_sharing():
-    data_np = np.random.randn(16, 32, 32, 16).astype("float16")
-    weight1_np = np.random.randn(16, 3, 3, 16).astype("float16")
-    weight2_np = np.random.randn(16, 3, 3, 16).astype("float16")
-
-    out = get_result_with_relax_cutlass_offload(Conv2dx2, data_np, weight1_np, weight2_np)
-
-    relay_expr = get_relay_conv2d_relu_x2(data_np.shape, weight1_np.shape)
-    ref = get_relay_ref(relay_expr, data_np, weight1_np, weight2_np)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-5, atol=1e-5)
-
-
-def test_matmul_transposed_offload(matmul_x, matmul_y):
-    x, y = matmul_x, matmul_y
-
-    mod = get_relax_matmul_module(x, y.transpose(), transposed_y=True)
-    out = get_result_with_relax_cutlass_offload(mod, x, y.transpose())
-    ref_relay_expr = get_relay_matmul(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose())
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
-
-
-def test_matmul_transposed_bias_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
-
-    mod = get_relax_matmul_module(
-        x, y.transpose(), transposed_y=True, with_bias=True, activation=None
-    )
-    out = get_result_with_relax_cutlass_offload(mod, x, y.transpose(), bias)
-
-    ref_relay_expr = get_relay_matmul_bias(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
+@pytest.mark.parametrize(
+    "x_shape, y_shape, expected",
+    [
+        # Regular matmul
+        ((3, 4), (4, 5), True),
+        # Batch matmul without stretching
+        ((3, 16, 15), (3, 15, 2), True),
+        # Broadcast 2D to 3D
+        ((3, 16, 15), (15, 2), True),
+        ((16, 15), (3, 15, 2), True),
+        # Broadcast one-length dimension
+        ((1, 16, 15), (3, 15, 2), True),
+        ((3, 16, 15), (1, 15, 2), True),
+        ((1, 1, 16, 15), (3, 2, 4, 15, 2), True),
+        # ND x ND
+        ((3, 2, 4, 16, 15), (3, 2, 4, 15, 2), True),
+        # ND x ND with one-length dimension
+        ((1, 2, 4, 16, 15), (1, 2, 4, 15, 2), True),
+        ((3, 2, 1, 16, 15), (3, 2, 1, 15, 2), True),
+        # Extra one-length dimension doesn't block broadcasting
+        ((3, 2, 1, 16, 15), (1, 1, 3, 2, 1, 15, 2), True),
+        # Not broadcasting all dims. Cannot be computed by stride-based batch gemm
+        ((3, 1, 1, 16, 15), (3, 2, 4, 15, 2), False),
+        ((3, 2, 4, 16, 15), (2, 4, 15, 2), False),
+        # Different shape
+        ((3, 4, 16, 15), (3, 2, 15, 2), False),
+    ],
+)
+def test_is_valid_for_cutlass_matmul(x_shape, y_shape, expected):
+    assert is_valid_for_cutlass_matmul(x_shape, y_shape) == expected
 
 
-def test_matmul_transposed_bias_relu_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
+@pytest.mark.parametrize(
+    "x_shape, y_shape, transpose_y, dtype",
+    [
+        # Not broadcasting all dims. Cannot be computed by stride-based batch gemm
+        ((3, 1, 1, 16, 15), (3, 2, 4, 15, 2), False, "float16"),
+        ((1, 2, 1, 16, 15), (2, 1, 4, 15, 2), False, "float16"),
+        ((3, 2, 4, 16, 15), (2, 4, 15, 2), True, "float16"),
+        ((3, 16, 15), (2, 1, 3, 15, 2), True, "float16"),
+        # Unsupported dtype
+        ((4, 8), (8, 4), False, "float32"),
+        ((5, 4, 8), (8, 4), True, "float32"),
+    ],
+)
+def test_cutlass_partition_matmul_blocked(x_shape, y_shape, transpose_y, dtype):
+    x = np.random.randn(*x_shape).astype(dtype)
+    y = np.random.randn(*y_shape).astype(dtype)
+    if transpose_y:
+        y = np.swapaxes(y, -2, -1)
 
-    mod = get_relax_matmul_module(
-        x, y.transpose(), transposed_y=True, with_bias=True, activation=R.nn.relu
-    )
-    out = get_result_with_relax_cutlass_offload(mod, x, y.transpose(), bias)
+    mod = get_relax_matmul_module(x, y, with_bias=False, transposed_y=transpose_y)
 
-    ref_relay_expr = get_relay_matmul_bias_relu(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-3, atol=1e-4)
-
-
-def test_matmul_transposed_bias_gelu_offload(matmul_x, matmul_y, matmul_bias):
-    x, y, bias = matmul_x, matmul_y, matmul_bias
-
-    mod = get_relax_matmul_module(
-        x, y.transpose(), transposed_y=True, with_bias=True, activation=R.nn.gelu
-    )
-    out = get_result_with_relax_cutlass_offload(mod, x, y.transpose(), bias)
-
-    ref_relay_expr = get_relay_matmul_bias_gelu(x.shape, y.shape[::-1])
-    ref = get_relay_ref(ref_relay_expr, x, y.transpose(), bias)
-
-    tvm.testing.assert_allclose(out, ref, rtol=1e-2, atol=1e-3)
+    tvm.ir.assert_structural_equal(mod, partition_for_cutlass(mod))
 
 
 if __name__ == "__main__":
